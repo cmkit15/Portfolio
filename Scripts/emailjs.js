9d224825c40e40ae9565e1e2c1580adf365722e1
cmkit15 <cmkit15@gmail.com>
2020-10-01 12:05:32 -0400

    Emailjs installation
    Emailjs initial install.

---
+++ Scripts/emailjs.js
@@ -0,0 +1,20 @@
+﻿const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'sending...';
+
+        const serviceID = 'service_p4f9gf5';
+        const templateID = 'template_l9ns9kh';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+        })
