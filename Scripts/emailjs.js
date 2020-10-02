39668f71faf606db735ebb1ff4523e76175f0dce
cmkit15 <cmkit15@gmail.com>
2020-10-02 14:13:38 -0400

    Portfolio updates
    Finishing touches on portfolio before submission.

--- Scripts/emailjs.js
+++ Scripts/emailjs.js
@@ -1,24 +1,41 @@
 ﻿const btn = document.getElementById('button');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let phone = document.getElementById('phone');
-let message = document.getElementById('message');
+
 
 document.getElementById('form')
     .addEventListener('submit', function (event) {
         event.preventDefault();
 
-        btn.value = 'sending...';
+        //let name = document.getElementById('name').value;
+        //let email = document.getElementById('email').value;
+        //let phone = document.getElementById('phone').value;
+        //let message = document.getElementById('message').value;
 
-        const serviceID = 'service_p4f9gf5';
-        const templateID = 'template_l9ns9kh';
+        //if (name == "" || email == "" || phone == "" || message == "") {
+        //    alert("You must fill out the entire form.");
+        //    return false;
+        //} else {
+            btn.value = 'sending...';
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Send Email';
-                alert('Your message has been sent!');
-            }, (err) => {
-                btn.value = 'Send Email';
-                alert(JSON.stringify(err));
-            });
-        })
+            const serviceID = 'service_p4f9gf5';
+            const templateID = 'template_l9ns9kh';
+
+            emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    btn.value = 'Send Email';
+                    Swal.fire(
+                        'Good news',
+                        'Your email has been sent',
+                        'success'
+                    )
+                    document.getElementById('name').value = "";
+                    document.getElementById('email').value = "";
+                    document.getElementById('phone').value = "";
+                    document.getElementById('message').value = "";
+                }, (err) => {
+                    btn.value = 'Send Email';
+                    alert(JSON.stringify(err));
+                });
+        // }            
+        //}
+    })
+
