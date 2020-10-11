5f246362c48279267ae067eff29ba086efff70f1
cmkit15 <cmkit15@gmail.com>
2020-10-11 16:51:20 -0400

    Template updates
    Template updates.

--- Scripts/emailjs.js
+++ Scripts/emailjs.js
@@ -2,39 +2,42 @@
 
 
 document.getElementById('form').addEventListener('submit', function (event) {
-        event.preventDefault();
+    event.preventDefault();
+    
+        sendEmail();
+        
+})
 
-        //let name = document.getElementById('name').value;
-        //let email = document.getElementById('email').value;
-        //let phone = document.getElementById('phone').value;
-        //let message = document.getElementById('message').value;
+function sendEmail() {
+    btn.value = 'sending...';
 
-        //if (name == "" || email == "" || phone == "" || message == "") {
-        //    alert("You must fill out the entire form.");
-        //    return false;
-        //} else {
-            btn.value = 'sending...';
+    const serviceID = 'service_p4f9gf5';
+    const templateID = 'template_l9ns9kh';
 
-            const serviceID = 'service_p4f9gf5';
-            const templateID = 'template_l9ns9kh';
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            Swal.fire(
+                'Good news',
+                'Your email has been sent',
+                'success'
+            )
+            document.getElementById('name').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('phone').value = "";
+            document.getElementById('message').value = "";
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+}
 
-            emailjs.sendForm(serviceID, templateID, this)
-                .then(() => {
-                    btn.value = 'Send Email';
-                    Swal.fire(
-                        'Good news',
-                        'Your email has been sent',
-                        'success'
-                    )
-                    document.getElementById('name').value = "";
-                    document.getElementById('email').value = "";
-                    document.getElementById('phone').value = "";
-                    document.getElementById('message').value = "";
-                }, (err) => {
-                    btn.value = 'Send Email';
-                    alert(JSON.stringify(err));
-                });
-        // }            
-        //}
-    })
-
+//let name = document.getElementById('name').value;
+//let email = document.getElementById('email').value;
+//let phone = document.getElementById('phone').value;
+//let message = document.getElementById('message').value;
+//
+//if (name == "" || email == "" || phone == "" || message == "") {
+//    alert("You must fill out the entire form.");
+//    return false;
+//}
